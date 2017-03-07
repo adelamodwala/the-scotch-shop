41c7dc760f61ad30823ab4e28d762c67e508ed11
adel.amodwala <adel.amodwala@ethoca.com>
2017-03-07 10:47:30 -0500

    update webpack prod to match dev

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -20,6 +20,10 @@ module.exports = {
                 'NODE_ENV': JSON.stringify('production')
             }
         }),
+        new webpack.ProvidePlugin({
+            'Promise': 'es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compressor: {
                 warnings: false
@@ -29,15 +33,11 @@ module.exports = {
     module: {
         loaders: [{
             test: /\.scss$/,
-            loaders: ["style", "css", "sass"]
+            use: ["style-loader", "css-loader", "sass-loader"]
         }, {
             test: /\.jsx?/,
-            loaders: ['babel'],
+            use: ['babel-loader'],
             include: path.join(__dirname, 'src')
-        }, {test: /\.json$/, loader: 'json'}]
-    },
-    sassLoader: {
-        // Apply the JSON importer via sass-loader's options.
-        importer: jsonImporter
+        }]
     }
 };
